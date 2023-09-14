f7a04bdaea8fe5335d30582342482d59418ee341
Virtual Items <virtualitemsuniverse@gmail.com>
2023-09-14 18:23:43 -0500

    updated file trying.js

--- scripts/trying.js
+++ scripts/trying.js
@@ -37,12 +37,18 @@ if (!window.ReactDOM) {
   return;
 }
 
+if (!window.crypto) {
+  console.error('Crypto is not loaded');
+  return;
+}
+
 
 // ----------------------------------------
 // Globals
 // ----------------------------------------
 
 const {
+  crypto,
   document,
   React,
   ReactDOM,
@@ -53,6 +59,8 @@ const {
   createElement: ce,
   Fragment,
   StrictMode,
+  useEffect,
+  useState,
 
 } = React;
 
@@ -62,15 +70,18 @@ const {
   Col,
   Container,
   Form,
+  Modal,
   Row,
 
 } = ReactBootstrap;
 
-const nbsp = '\u00A0';
+const unicode = {
+  nbsp: '\u00A0',
+};
 
 const root = document.getElementById(rid);
 
-const shadowRoot = root.attachShadow({mode: 'open'});
+const shadowRoot = root.attachShadow({mode: 'closed'});
 
 const rootAttributes = {
   bootstrapCss: root.getAttribute('bootstrap-css'),
@@ -78,6 +89,7 @@ const rootAttributes = {
   customCss: root.getAttribute('custom-css'),
   fontAwesomeCss: root.getAttribute('font-awesome-css'),
   tituloEvaluacion: root.getAttribute('titulo-evaluacion'),
+  requestUuid: root.getAttribute('request-uuid'),
 };
 
 
@@ -96,10 +108,49 @@ const icon = function(className) {
 };
 
 
+function debounce(func, timeout=1000) {
+  let timer;
+
+  const wrapper = function() {
+    const thisArg = this;
+    const args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(func.bind(thisArg, ...args), timeout);
+  };
+
+  return wrapper;
+};
+
+
 // ----------------------------------------
 // Hooks
 // ----------------------------------------
 
+function useKeyGenerator() {
+  const [keys, setKeys] = useState([]);
+
+  function next() {
+    const key = crypto.randomUUID();
+    setKeys([...keys, key]);
+    return key;
+  };
+
+  return [keys, next];
+};
+
+
+function useSerialGenerator(start=0) {
+  const _start = Number(start);
+
+  const [serial, setSerial] = useState(_start);
+
+  function next() {
+    setSerial(serial + 1);
+    return serial;
+  };
+
+  return [serial, next];
+};
 
 
 // ----------------------------------------
@@ -112,6 +163,112 @@ const icon = function(className) {
 // Models
 // ----------------------------------------
 
+const ModeloBase = class {
+  constructor(key=null) {
+    this.key = key;
+  }
+};
+
+const ModeloContenidoTexto = class extends ModeloBase {
+  constructor(texto='', key) {
+    super(key);
+    this.texto = texto;
+  }
+};
+
+
+const ModeloContenidoEnlace = class extends ModeloBase {
+  constructor(url='', texto='', key) {
+    super(key);
+    this.url = url;
+    this.texto = texto;
+  }
+};
+
+
+const ModeloContenidoImagen = class extends ModeloBase {
+  constructor(archivo=null, key) {
+    super(key);
+    this.archivo = archivo;
+  }
+};
+
+
+const ModeloContenidoPDF = class extends ModeloBase {
+  constructor(archivo=null, key) {
+    super(key);
+    this.archivo = archivo;
+  }
+};
+
+
+const ModeloOpcionPreguntaSeleccion = class extends ModeloBase {
+  constructor(descripcion='', correcta=false, key) {
+    super(key);
+    this.descripcion = descripcion;
+    this.correcta = correcta;
+  }
+};
+
+
+const ModeloPreguntaSeleccion = class extends ModeloBase {
+  constructor(opciones=[], puntaje=0, requerida=true, key) {
+    super(key);
+    this.opciones = opciones;
+    this.puntaje = puntaje;
+    this.requerida = requerida;
+  }
+};
+
+
+const ModeloPreguntaTexto = class extends ModeloBase {
+  constructor(puntaje=0, requerida=true, key) {
+    super(key);
+    this.puntaje = puntaje;
+    this.requerida = requerida;
+  }
+};
+
+
+const ModeloElemento = class extends ModeloBase {
+
+  contenido = {
+    texto: Symbol('texto'),
+    enlace: Symbol('enlace'),
+    imagen: Symbol('imagen'),
+    pdf: Symbol('pdf'),
+  };
+
+  pregunta = {
+    seleccion: Symbol('seleccion'),
+    texto: Symbol('texto'),
+  };
+
+  constructor(orden, tipo='', objeto=null, key) {
+    super(key);
+    this.orden = orden;
+    this.tipo = tipo;
+    this.objeto = objeto;
+  }
+
+};
+
+
+const ModeloModulo = class extends ModeloBase {
+  constructor(nombre='', elementos=[], key) {
+    super(key);
+    this.nombre = nombre;
+    this.elementos = elementos;
+  }
+};
+
+
+const ModeloEvaluacion = class extends ModeloBase {
+  constructor(modulos=[], key) {
+    super(key);
+    this.modulos = modulos;
+  }
+};
 
 
 // ----------------------------------------
@@ -127,6 +284,10 @@ const blockButton = function({children, ...props}) {
   return ce('div', { className: 'd-grid gap-2' }, ce(Button, props, children));
 };
 
+
+const Enunciado = function({ children, ...props }) {
+  return ce('div', props, ce('span', null, children));
+};
 
 
 // ----------------------------------------
@@ -147,37 +308,102 @@ const DosColumnas = function({ izquierda, derecha }) {
 
 };
 
-/*
-<Card>
-  <Card.Header>Featured</Card.Header>
-  <Card.Body>
-    <Card.Title>Special title treatment</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-*/
 
-/*
+const AgregarContenidoModal = function({ children, mostrando, fnOcultar, ...props }) {
+
+  const modal = (
+    ce(Modal, { show: mostrando, onHide: fnOcultar, container: shadowRoot },
+      ce(Modal.Header, { closeButton: true },
+        ce(Modal.Title, null, 'Agregar contenido')
+      ),
+      ce(Modal.Body, null, 'Woohoo, you are reading this text in a modal!'),
+      ce(Modal.Footer, null,
+        ce(Button, { variant: 'secondary', onClick: fnOcultar }, 'Close'),
+        ce(Button, { variant: 'primary', onClick: fnOcultar }, 'Save Changes')
+      )
+    )
+  );
+
+  return ce(Fragment, null, modal);
+};
 
 
+const AgregarPreguntaModal = function({ children, mostrando, fnOcultar, ...props }) {
 
-*/
+  const modal = (
+    ce(Modal, { show: mostrando, onHide: fnOcultar, container: shadowRoot },
+      ce(Modal.Header, { closeButton: true },
+        ce(Modal.Title, null, 'Agregar contenido')
+      ),
+      ce(Modal.Body, null, 'Woohoo, you are reading this text in a modal!'),
+      ce(Modal.Footer, null,
+        ce(Button, { variant: 'secondary', onClick: fnOcultar }, 'Close'),
+        ce(Button, { variant: 'primary', onClick: fnOcultar }, 'Save Changes')
+      )
+    )
+  );
 
-const Modulo = function({ elementos = [] }) {
+  return ce(Fragment, null, modal);
+
+};
+
+
+const Modulo = function({
+    elementos = [],
+    fnActualizar = () => {},
+    fnQuitar = () => {},
+    ...props
+  }) {
+
+  const campoNombre = ce(Form.Control, { type: 'text', onChange: (e) => fnActualizar({ nombre: e.target.value }) });
 
   const title = ce(Fragment, null,
     ce(Col, { xs: 3, className: 'mb-4' }, ce('span', { className: 'h3' }, 'Nombre del módulo:')),
-    ce(Col, { xs: 9, className: 'mb-4' }, ce(Form.Control, { type: 'text' })),
+    ce(Col, { xs: 9, className: 'mb-4' }, campoNombre),
   );
 
-  const filasElementos = elementos.map((elemento, i) => ce(Col, { key: i, xs: 12, className: 'mb-3' }, elemento));
+  const filasElementos = elementos.map((elemento, i) => ce(Col, { key: i, xs: 12, className: 'mb-3' }, elemento, ce('hr')));
 
-  const btnAgregarContenidos = ce(blockButton, { variant: 'primary', size: 'lg'}, icon('fa fa-plus'), ' Agregar contenidos');
+  const [mostrandoModalContenido, cambiarMostrandoModalContenido] = useState(false);
 
-  const btnAgregarCampos = ce(blockButton, { variant: 'primary', size: 'lg'}, icon('fa fa-plus'), ' Agregar preguntas');
+  const modalContenido = ce(AgregarContenidoModal, { mostrando: mostrandoModalContenido, fnOcultar: () => cambiarMostrandoModalContenido(false) });
+
+  const [mostrandoModalPregunta, cambiarMostrandoModalPregunta] = useState(false);
+
+  const modalPregunta = AgregarPreguntaModal({ mostrando: mostrandoModalPregunta, fnOcultar: () => cambiarMostrandoModalPregunta(false) });
+
+  const btnAgregarContenidos = ce(blockButton,
+    {
+      variant: 'primary',
+      size: 'lg',
+      onClick: (e) => cambiarMostrandoModalContenido(true),
+    },
+    icon('fa fa-plus'),
+    ' Agregar contenidos'
+  );
+
+  const btnAgregarCampos = ce(blockButton,
+    {
+      variant: 'primary',
+      size: 'lg',
+      onClick: (e) => cambiarMostrandoModalPregunta(true),
+    },
+    icon('fa fa-plus'),
+    ' Agregar preguntas'
+  );
+
+  const btnEliminarModulo = (
+    ce(iconButton,
+      {
+        className: 'position-absolute top-0 end-0',
+        iconName: 'fa fa-trash',
+        size: 'sm',
+        variant: 'danger',
+        onClick: (e) => fnQuitar(),
+      },
+      ' Eliminar módulo'
+    )
+  );
 
   const botones = ce(Fragment, null,
     ce(Col, { xs: 6, className: 'mb-3' }, btnAgregarContenidos),
@@ -185,12 +411,18 @@ const Modulo = function({ elementos = [] }) {
   );
 
   const panel = (
-    ce(Card, null,
+    ce(Card,
+      {
+        className: 'shadow px-3 py-5 mb-5 bg-body rounded',
+      },
+      btnEliminarModulo,
       ce(Card.Body, null,
         ce(Row, null,
           title,
           filasElementos,
           botones,
+          modalContenido,
+          modalPregunta,
         )
       )
     )
@@ -229,7 +461,7 @@ const PreguntaSeleccion = function({ opciones = [] }) {
 
   // Columna izquierda
 
-  const listaOpcionesConLinea = opciones.map((opcion, i) => ce(Fragment, { key: i }, opcion, ce('hr')))
+  const listaOpcionesConLinea = opciones.map((opcion, i) => ce('div', { key: i, className: 'mb-2' }, opcion));
 
   const listaOpciones = ce(Col,
     {
@@ -258,7 +490,7 @@ const PreguntaSeleccion = function({ opciones = [] }) {
     )
   );
 
-  const espacio = ce(Col, { xs: 12, className: 'mb-3' }, ce('div', null, nbsp));
+  const espacio = ce(Col, { xs: 12, className: 'mb-3' }, ce('div', null, unicode.nbsp));
 
   const puntaje = ce(Col, { xs: 12, className: 'mb-4' }, ce(Form.Control, { type: 'number', placeholder: 'Puntaje' }));
 
@@ -306,7 +538,7 @@ const PreguntaTexto = function() {
     )
   );
 
-  const espacio = ce(Col, { xs: 12, className: 'mb-3' }, ce('div', null, nbsp));
+  const espacio = ce(Col, { xs: 12, className: 'mb-3' }, ce('div', null, unicode.nbsp));
 
   const puntaje = ce(Col, { xs: 12, className: 'mb-4' }, ce(Form.Control, { type: 'number', placeholder: 'Puntaje' }));
 
@@ -321,14 +553,16 @@ const PreguntaTexto = function() {
 };
 
 
-const ContenidoTexto = function() {
+const ContenidoTexto = function({ children, ...props }) {
   // texto
 
   // Columna izquierda
 
-  const campo = ce(Form.Control, { as: 'textarea', type: 'text', placeholder: 'Escribe aquí el texto', className: 'h-100' });
+  const campo = ce(Form.Control, { as: 'textarea', type: 'text', placeholder: 'Escribe aquí el texto', rows: 5 });
 
-  const columnaIzquierda = ce(Col, { xs: 11 }, campo);
+  const enunciado = ce(Enunciado, { className: 'mb-3' }, 'Contenido de texto:');
+
+  const columnaIzquierda = ce(Col, { xs: 11 }, enunciado, campo);
 
   // Columna derecha
 
@@ -345,13 +579,13 @@ const ContenidoTexto = function() {
     )
   );
 
-  const espacio = ce(Col, { xs: 12, className: 'mb-3' }, ce('div', null, nbsp));
+  const espacio = ce(Col, { xs: 12, className: 'mb-3' }, ce('div', null, unicode.nbsp));
 
   const columnaDerecha = ce(Col, { xs: 1 }, ce(Row, null, eliminador, espacio));
 
   // unión
 
-  return ce(DosColumnas, { izquierda: columnaIzquierda, derecha: columnaDerecha });
+  return ce(DosColumnas, { ...props, izquierda: columnaIzquierda, derecha: columnaDerecha });
 
 };
 
@@ -362,11 +596,13 @@ const ContenidoEnlace = function() {
 
   // Columna izquierda
 
-  const campoEnlace = ce(Form.Control, { type: 'text', className: 'mb-3', placeholder: 'Escribe aquí el enlace' });
+  const campoEnlace = ce(Form.Control, { type: 'text', className: 'mb-3', placeholder: 'Escribe aquí la URL' });
 
-  const campoTexto = ce(Form.Control, { type: 'text', placeholder: 'Escribe aquí el texto' });
+  const campoTexto = ce(Form.Control, { type: 'text', placeholder: 'Escribe aquí el texto del enlace' });
 
-  const columnaIzquierda = ce(Col, { xs: 11 }, campoEnlace, campoTexto);
+  const enunciado = ce(Enunciado, { className: 'mb-3' }, 'Contenido de enlace:');
+
+  const columnaIzquierda = ce(Col, { xs: 11 }, enunciado, campoEnlace, campoTexto);
 
   // Columna derecha
 
@@ -383,7 +619,7 @@ const ContenidoEnlace = function() {
     )
   );
 
-  const espacio = ce(Col, { xs: 12, className: 'mb-3' }, ce('div', null, nbsp));
+  const espacio = ce(Col, { xs: 12, className: 'mb-3' }, ce('div', null, unicode.nbsp));
 
   const columnaDerecha = ce(Col, { xs: 1 }, ce(Row, null, eliminador, espacio));
 
@@ -397,11 +633,73 @@ const ContenidoEnlace = function() {
 const ContenidoImagen = function() {
   // archivo
 
+  // Columna izquierda
+
+  const campoArchivo = ce(Form.Control, { type: 'file', className: 'mb-3', accept: 'image/*' });
+
+  const enunciado = ce(Enunciado, { className: 'mb-3' }, 'Contenido de imágen:');
+
+  const columnaIzquierda = ce(Col, { xs: 11 }, enunciado, campoArchivo);
+
+  // Columna derecha
+
+  const eliminador = (
+    ce(Col, null,
+      ce(iconButton,
+        {
+          size: 'sm',
+          iconName: 'fa fa-trash',
+          variant: 'danger',
+          className: 'position-absolute top-0 end-0',
+        }
+      )
+    )
+  );
+
+  const espacio = ce(Col, { xs: 12, className: 'mb-3' }, ce('div', null, unicode.nbsp));
+
+  const columnaDerecha = ce(Col, { xs: 1 }, ce(Row, null, eliminador, espacio));
+
+  // unión
+
+  return ce(DosColumnas, { izquierda: columnaIzquierda, derecha: columnaDerecha });
+
 };
 
 
 const ContenidoPDF = function() {
   // archivo
+
+  // Columna izquierda
+
+  const campoArchivo = ce(Form.Control, { type: 'file', className: 'mb-3', accept: 'application/pdf' });
+
+  const enunciado = ce(Enunciado, { className: 'mb-3' }, 'Contenido de documento PDF:');
+
+  const columnaIzquierda = ce(Col, { xs: 11 }, enunciado, campoArchivo);
+
+  // Columna derecha
+
+  const eliminador = (
+    ce(Col, null,
+      ce(iconButton,
+        {
+          size: 'sm',
+          iconName: 'fa fa-trash',
+          variant: 'danger',
+          className: 'position-absolute top-0 end-0',
+        }
+      )
+    )
+  );
+
+  const espacio = ce(Col, { xs: 12, className: 'mb-3' }, ce('div', null, unicode.nbsp));
+
+  const columnaDerecha = ce(Col, { xs: 1 }, ce(Row, null, eliminador, espacio));
+
+  // unión
+
+  return ce(DosColumnas, { izquierda: columnaIzquierda, derecha: columnaDerecha });
 
 };
 
@@ -410,40 +708,76 @@ const ContenidoPDF = function() {
 // Composites
 // ----------------------------------------
 
-const Cabecera = function() {
-  return ce('header', null,
+const Cabecera = function({children, ...props}) {
+  return ce('header', props,
     ce('h1', null, rootAttributes.tituloEvaluacion)
   );
 };
 
 
-const ManejadorDeModulos = function({ modulos = [], ...props }) {
+const ManejadorDeModulos = function({ ...props }) {
 
-  const listaModulos = [
-    ce(Modulo, { key: 1, elementos: [
-      ce(PreguntaTexto),
-      ce(PreguntaSeleccion, {
-        opciones: [
-          ce(OpcionPreguntaSeleccion),
-          ce(OpcionPreguntaSeleccion),
-          ce(OpcionPreguntaSeleccion)
-        ]
-      }),
-      ce(PreguntaTexto),
+  const [listaModulos, guardarModulos] = useState([]);
 
-    ] }),
-    // ce(Modulo, { key: 2 }),
-    // ce(Modulo, { key: 3 }),
-  ];
+  const [serialModulos, siguienteSerialModulos] = useSerialGenerator();
 
-  const btnAgregar = ce(blockButton, { variant: 'success', size: 'lg' }, icon('fa fa-plus'), ' Agregar módulo');
+  const agregarModulo = function(key) {
+    const modulo = new ModeloModulo('', [], key);
+    const modulos = [...listaModulos, modulo];
+    guardarModulos(modulos);
+  };
+
+  const quitarModulo = function(key) {
+    const modulos = listaModulos.filter(modulo => modulo.key !== key);
+    guardarModulos(modulos);
+  };
+
+  const actualizarModulo = function(key, datos) {
+    const indice = listaModulos.findIndex(modulo => modulo.key === key);
+
+    if (datos.nombre)
+      listaModulos[indice].nombre = datos.nombre;
+
+    if (datos.elementos)
+      listaModulos[indice].elementos = datos.elementos;
+
+      guardarModulos(listaModulos);
+  }
+
+  const eventoAgregarModulo = function() {
+    const key = siguienteSerialModulos();
+    agregarModulo(key);
+  };
+
+  const btnAgregar = ce(blockButton,
+    {
+      variant: 'success',
+      size: 'lg',
+      onClick: eventoAgregarModulo,
+    },
+    icon('fa fa-plus'),
+    ' Agregar módulo'
+  );
+
+  const componentesModulos = listaModulos.map(
+    function(modulo) {
+      return ce(Modulo, {
+        key: modulo.key,
+        elementos: modulo.elementos,
+        fnActualizar: (datos) => actualizarModulo(modulo.key, datos),
+        fnQuitar: () => quitarModulo(modulo.key)
+      })
+    }
+  );
 
   const panel = (
-    ce(Row, {},
-      ce(Col, { xs: 12, className: 'mb-3' }, listaModulos),
+    ce(Row, { ...props },
+      ce(Col, { xs: 12, className: 'mb-3' }, componentesModulos),
       ce(Col, { xs: 12, className: 'mb-3' }, btnAgregar),
     )
   );
+
+  console.log(listaModulos);
 
   return panel;
 
@@ -479,9 +813,9 @@ const Layout = function({north, center, south}) {
 
 const View = function() {
   // composites
-  const north = ce(Cabecera, null, 'Norte');
-  const center = ce(ManejadorDeModulos, null, 'Centro');
-  const south = ce(BotonesFormulario, null, 'Sur');
+  const north = ce(Cabecera, { className: 'mb-5' });
+  const center = ce(ManejadorDeModulos, { className: 'mb-5' });
+  const south = ce(BotonesFormulario);
 
   // layout
   return ce(Fragment, null, ce(Layout, { north, center, south } ));
@@ -508,7 +842,7 @@ shadowRoot.appendChild(ext);
 
 ext = document.createElement('link');
 ext.rel = 'stylesheet';
-ext.href = rootAttributes.customCss;
+ext.href = [rootAttributes.customCss, '?v=', rootAttributes.requestUuid].join('');
 
 shadowRoot.appendChild(ext);
 
