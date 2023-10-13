456488b19389ca915f21d1dcedce00ca1f7d15f4
Alejandro CR <contacto@alejandrocr.co>
2023-10-13 14:45:51 -0500

    crated shadow root example

---
+++ browsers/shadowroot.js
@@ -0,0 +1,17 @@
+const rootElement = document.getElementById('root');
+
+const shadow = rootElement.attachShadow({mode: 'open'});
+
+const style = document.createElement('style');
+style.textContent = `
+  h1 {
+    color: red;
+    font-family: sans-serif;
+  }
+`;
+
+const heading = document.createElement('h1');
+heading.textContent = 'Shadow DOM';
+
+shadow.appendChild(style);
+shadow.appendChild(heading);
