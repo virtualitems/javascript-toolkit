a3054c96abf862fd0252f6b317ce753c92abe13b
Alejandro CR <contacto@alejandrocr.co>
2023-10-13 13:51:36 -0500

    created websocket example file

---
+++ browsers/websocket.js
@@ -0,0 +1,25 @@
+// promise web sockets
+(function() {
+
+  const socket = new WebSocket('wss://example.com');
+
+  socket.addEventListener('open', function(event) {
+    console.log('Connection established');
+
+    socket.send('hello world!');
+    socket.close();
+  });
+
+  socket.addEventListener('message', function(event) {
+    console.log('Message received', event.data);
+  });
+
+  socket.addEventListener('close', function(event) {
+    console.log('Connection closed');
+  });
+
+  socket.addEventListener('error', function(error) {
+    console.error('Error', error);
+  });
+
+})();
