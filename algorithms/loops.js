45f2b59a878f58b88396737ec4064b6ff1c0505e
Alejandro CR <contacto@alejandrocr.co>
2023-10-14 22:24:19 -0500

    created loops examples

---
+++ algorithms/loops.js
@@ -0,0 +1,87 @@
+// iterables
+const list = [1, 2, 3, 4];
+const object = {one: 1, two: 2, three: 3, four: 4};
+
+
+// for
+(function () {
+
+console.warn('for');
+
+for (let i = 0, end = list.length; i < end; i++) {
+  const element = list[i];
+  console.log(element);
+}
+
+})();
+
+
+// for of
+(function () {
+
+console.warn('for of');
+
+for (const element of list) {
+  console.log(element);
+}
+
+})();
+
+
+// for in
+(function () {
+
+console.warn('for in');
+
+for (const key in object) {
+  const element = object[key];
+  console.log(element);
+}
+
+})();
+
+
+// while
+(function () {
+
+console.warn('while');
+
+let index = 0;
+const end = list.length;
+
+while (index < end) {
+  const element = list[index];
+  console.log(element);
+  index++;
+}
+
+})();
+
+
+// do while
+(function () {
+
+console.warn('do while');
+
+let index = 0;
+const end = list.length;
+
+do {
+  const element = list[index];
+  console.log(element);
+  index++;
+} while (index < end);
+
+})();
+
+
+// forEach
+(function () {
+
+console.warn('forEach');
+
+list.forEach((element) => {
+  console.log(element);
+});
+
+})();
