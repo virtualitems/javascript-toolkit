bc291adfb63c99cbaa21945901847359dcdc17b5
Virtual Items <virtualitemsuniverse@gmail.com>
2023-10-24 17:02:03 -0500

    updated coalesce.js Added isValuable function

--- algorithms/coalesce.js
+++ algorithms/coalesce.js
@@ -5,16 +5,15 @@
  */
 const coalesce = function() {
   const args = Array.from(arguments);
-  const is = Object.is;
 
   for(let i=0, end=args.length; i<end; i++) {
 
     const arg = args[i];
 
     if (
-      !is(arg, undefined) &&
-      !is(arg, null) &&
-      !is(arg, NaN)
+      !arg === undefined &&
+      !arg === null &&
+      !Object.is(arg, NaN)
     ) {
       return arg;
     }
@@ -22,5 +21,30 @@ const coalesce = function() {
   }
 
   return null;
+
+};
+
+
+/**
+ * Check if a value is valuable
+ *
+ * @param {any} value
+ * @returns {Boolean}
+ */
+const isValuable = function(value) {
+
+  if (value === undefined) {
+    return false;
+  }
+
+  if (value === null) {
+    return false;
+  }
+
+  if(Object.is(value, NaN)) {
+    return false;
+  }
+
+  return true;
 
 };
