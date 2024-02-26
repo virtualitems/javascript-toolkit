7d33b8313fc758037e277cc2c12859c280a2e65e
Alejandro CR <contacto@alejandrocr.co>
2024-02-26 00:44:24 -0500

    updated reactjs template with ladder architecture

--- browsers/libraries/reactjs/template.js
+++ browsers/libraries/reactjs/template.js
@@ -1,54 +1,62 @@
 /**
  * React Application
  *
- * @requires React
- * @requires React-DOM
+ * @requires react
+ * @requires react-dom
  * @see https://react.dev/
  *
+ * @requires remix-run-router
+ * @requires react-router
+ * @requires react-router-dom
+ * @see https://reactrouter.com/
+ *
  * @param {HTMLElement} rootElement
- * @param {Object} React
- * @param {Object} ReactDOM
  * @param {Window} window
+ * @param {Object} window.React
+ * @param {Object} window.ReactDOM
+ * @param {Object} window.ReactRouterDOM
  */
-(function(rootElement, React, ReactDOM, window) {
+(function(rootElement, window) {
 'use strict';
-
 
 // ----------------------------------------
 // Checks
 // ----------------------------------------
 
-/**
- * Check if a value is valuable
- *
- * @param {any} value
- * @returns {Boolean}
- */
-const isValuable = (value) => (value !== undefined && value !== null && !Object.is(value, NaN));
-
-if (!isValuable(rootElement)) {
+if (! typeof rootElement === 'object') {
   throw new Error('Root element not found');
 }
 
-if (!isValuable(React)) {
-  throw new Error('React not found');
-}
-
-if (!isValuable(ReactDOM)) {
-  throw new Error('ReactDOM not found');
-}
-
-if (!isValuable(window)) {
+if (! typeof window === 'object') {
   throw new Error('Window not found');
 }
 
+if (! typeof window.document === 'object') {
+  throw new Error('window.document not found');
+}
+
+if (! typeof window.React === 'object') {
+  throw new Error('window.React not found');
+}
+
+if (! typeof window.ReactDOM === 'object') {
+  throw new Error('window.ReactDOM not found');
+}
+
+if (! typeof window.ReactRouterDOM === 'object') {
+  throw new Error('window.ReactRouterDOM not found');
+}
+
 
 // ----------------------------------------
-// Globals
+// Constants
 // ----------------------------------------
 
 const {
-  document
+  document,
+  React,
+  ReactDOM,
+  ReactRouterDOM,
 
 } = window;
 
@@ -58,6 +66,13 @@ const {
   StrictMode,
 
 } = React;
+
+const {
+  BrowserRouter,
+  Route,
+  Routes,
+
+} = ReactRouterDOM;
 
 
 // ----------------------------------------
@@ -91,22 +106,16 @@ const {
 
 
 // ----------------------------------------
-// Contents
+// Atomics
 // ----------------------------------------
 
-const Content = function(props) {
-  return ce('h1', props, 'React Application')
-};
-
-
-// ----------------------------------------
-// Containers
-// ----------------------------------------
-
-const Container = function(props) {
-  const { children } = props;
-
-  return ce('header', props, children);
+/**
+ * atomic component
+ * @param {Object} props
+ */
+function Title(props) {
+  // render
+  return ce('h1', props, props.children);
 };
 
 
@@ -114,34 +123,90 @@ const Container = function(props) {
 // Composites
 // ----------------------------------------
 
-const Composite = function(props) {
-  const content = ce(Content);
-  const container = ce(Container, null, content);
+/**
+ * content component
+ * @param {Object} props
+ */
+function Header(props) {
+  // contents
+  const title = ce(Title, null, 'Ladder Architecture!');
 
-  return ce('main', props, container);
-};
-
-
-// ----------------------------------------
-// Layout
-// ----------------------------------------
-
-const Layout = function(props) {
-  const { center } = props;
-
-  return ce(Fragment, null, center);
-};
+  // render
+  return ce('header', props, title);
+}
 
 
-// ----------------------------------------
-// View
-// ----------------------------------------
+/**
+ * Container component
+ * @param {Object} props
+ */
+function Layout(props) {
+  // slots
+  const center = props.center;
 
-const View = function(props) {
-  const center = ce(Composite);
-  const layout = ce(Layout, { center });
+  // render
+  return ce('section', null, center);
+}
 
+
+/**
+ * Content component
+ * @param {Object} props
+ */
+function HomePage(props) {
+  // contents
+  const header = ce(Header);
+
+  // containers
+  const layout = ce(Layout, { center: header });
+
+  // render
   return ce(Fragment, null, layout);
+}
+
+
+/**
+ * Container component
+ * @param {Object} props
+ */
+function Router(props) {
+  // slots
+  const routes = props.routes;
+
+  // render
+  return ce(BrowserRouter, null,
+    ce(Routes, null,
+      routes.map((route, key) => ce(Route,
+        {
+          key,
+          path: route.path,
+          element: route.element
+        }
+      ))
+    )
+  );
+}
+
+
+// ----------------------------------------
+// Bootstrap
+// ----------------------------------------
+
+/**
+ * Bootstrap component
+ * @param {Object} props
+ */
+const Bootstrap = function(props) {
+  // contents
+  const routes = [
+    { path: '/', element: ce(HomePage) }
+  ]
+
+  // containers
+  const router = ce(Router, { routes });
+
+  // render
+  return ce(StrictMode, null, router);
 };
 
 
@@ -150,12 +215,9 @@ const View = function(props) {
 // ----------------------------------------
 ReactDOM
   .createRoot(rootElement)
-  .render(ce(StrictMode, null, ce(View)));
-
+  .render(ce(Bootstrap));
 
 })(
   window.document.getElementById('root'),
-  window.React,
-  window.ReactDOM,
   window,
 );
