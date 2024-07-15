a1c9f3140c78f76d0a2c99dba906fbdc70f0c46d
Virtual Items <virtualitemsuniverse@gmail.com>
2024-07-15 14:11:09 -0500

    create class.template.js

---
+++ snippets/class.template.js
@@ -0,0 +1,52 @@
+// Libraries
+
+// Same Shared Module Layer
+
+// Lower Shared Module Layers
+
+// Other Modules
+
+// Same Layer
+
+// Lower Layers
+
+// Types
+
+// Interfaces
+
+// Constants
+
+
+/**
+ * @description 
+ */
+export default class __CLASS__ extends __OVERRIDE_FIRST_ABSTRACT_LAST__
+{
+
+  // public ATTRIBUTES
+
+  // protected ATTRIBUTES
+
+  // private ATTRIBUTES
+
+  // public static ATTRIBUTES
+
+  // protected static ATTRIBUTES
+
+  // private static ATTRIBUTES
+
+  // Constructor, Getters, Setters
+
+  // public METHODS
+
+  // protected METHODS
+
+  // private METHODS
+
+  // public static METHODS
+
+  // protected static METHODS
+
+  // private static METHODS
+
+} //:: class
