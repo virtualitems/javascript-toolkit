c22c79761e05f1e626a78a01e9f84e3c5ef3d432
Alejandro CR <contacto@alejandrocr.co>
2024-07-22 14:08:28 -0500

    refactor LazyComponent to use react hooks

--- libraries/browsers/reactjs/LazyComponent.js
+++ libraries/browsers/reactjs/LazyComponent.js
@@ -1,28 +1,30 @@
-const css = new CSSStyleSheet();
-
-css.replaceSync(`
-.lazy-component {
-  font-size: 3em;
-  text-align: center;
-  text-transform: uppercase;
-  text-shadow: #F55 1px 0 10px;
-  cursor: pointer;
-  transition: font-size 0.5s;
-}
-
-.lazy-component::before {
-  content: '🚀 ';
-}
-
-.lazy-component:hover {
-  font-size: 4em;
-}
-`);
-
-document.adoptedStyleSheets.push(css);
-
-
+/**
+ * @param {import('react').HTMLAttributes<HTMLElement>} props
+ * @returns {import('react').ReactElement}
+ */
 export default function LazyComponent(props)
 {
-  return React.createElement('div', { className: 'lazy-component' }, props.children);
+  const ref = React.useRef(null);
+
+  const [state, setState] = React.useState(false);
+
+  if (state === true) {
+    throw new Error('Error thrown in LazyComponent');
+  }
+
+  React.useEffect(
+    () => {
+      const el = ref.current;
+      el.style.cursor = 'pointer';
+      el.style.textAlign = 'center';
+      el.style.textTransform = 'uppercase';
+      el.style.textShadow = '#F55 1px 0 10px';
+      el.style.fontSize = '3em';
+    },
+    [ref.current]
+  );
+
+  const onClick = () => setState(true);
+
+  return React.createElement('div', { ref, onClick }, props.children);
 }
