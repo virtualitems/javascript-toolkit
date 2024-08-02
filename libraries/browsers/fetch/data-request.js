0a58288d8ef0541c538fb96293f2ed7633aed6ca
Alejandro CR <contacto@alejandrocr.co>
2024-08-02 01:29:56 -0500

    refactor class with a function in data-request.js

--- libraries/browsers/fetch/data-request.js
+++ libraries/browsers/fetch/data-request.js
@@ -4,89 +4,39 @@
  *
  * [javascript]
  *
- * const request = new DataRequest(target, {init, decoder, transformer});
+ * request(target, {init, decoder, transformer}).then(data => console.log(data));
  *
- * request.send().then(data => console.log(data));
  */
 
 
-class DataRequest {
+/**
+ * @param {RequestInfo | URL} target
+ * @param {object} options
+ * @param {RequestInit} options.init
+ * @param {(response: Response) => Promise<unknown>} options.decoder
+ * @param {(source: unknown) => Promise<unknown>} options.transformer
+ */
+async function request(target, options) {
 
-  /**
-   * @param {RequestInfo | URL} target
-   * @param {object} options
-   * @param {RequestInit} options.init
-   * @param {(response: Response) => Promise<unknown>} options.decoder
-   * @param {(source: unknown) => Promise<unknown>} options.transformer
-   */
-  constructor(target, options) {
-    this.target = target;
-    this.init = options.init;
-    this.decoder = options.decoder;
-    this.transformer = options.transformer;
+  // request -> response
+  const response = await fetch(target, options.init);
+
+  if (response.ok === false) {
+    throw new Error(`${response.status} ${response.statusText}`);
   }
 
-  /**
-   * @description request -> response
-   * @returns {Promise<Response>}
-   */
-  async request() {
-    try {
-      return await fetch(this.target, this.options);
-    } catch (error) {
-      console.warn(error);
-    }
+  if ('function' !== typeof options.decoder) {
+    return;
   }
 
-  /**
-   * @description response -> decoded source
-   * @param {Response} response
-   * @returns {Promise<unknown>}
-   */
-  async decode(response) {
-    try {
-      return await this.decoder(response);
-    } catch (error) {
-      console.warn(error);
-    }
+  // response -> decoded source
+  const source = await options.decoder(response);
+
+  if ('function' !== typeof options.transformer) {
+    return source;
   }
 
-  /**
-   * @description decoded source -> transformed data
-   * @param {unknown} source
-   * @returns {Promise<unknown>}
-   */
-  async transform(source) {
-    try {
-      return await this.transformer(source);
-    } catch (error) {
-      console.warn(error);
-    }
-  }
+  // decoded source -> transformed data
+  return await options.transformer(source);
 
-  async send() {
-    try {
-      const response = await this.request();
-
-      if (response.ok === false) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      }
-
-      if ('function' !== typeof this.decoder) {
-        return;
-      }
-
-      const source = await this.decode(response);
-
-      if ('function' !== typeof this.transformer) {
-        return source;
-      }
-
-      return await this.transform(source);
-
-    } catch (error) {
-      console.warn(error);
-    }
-  }
-
-}
+} //:: function request
