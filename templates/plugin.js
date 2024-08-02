28ea8f78c60bf07b66dec20b0dbf51f1c910b3db
Alejandro CR <contacto@alejandrocr.co>
2024-08-02 02:52:17 -0500

    move templates to tempalte dir

---
+++ templates/plugin.js
@@ -0,0 +1,79 @@
+/**
+ * @fileoverview Browser library template
+ * @version 0.0.1
+ */
+
+
+// ----------------------------------------
+// Checks
+// ----------------------------------------
+
+
+if (!window.alert) { // add dependency check
+  throw new Error('alert not found');
+}
+
+
+// ----------------------------------------
+// Namespaces
+// ----------------------------------------
+
+const namespace = 'Lib';
+
+
+if (namespace in window) { // check if library namespace is already defined
+  throw new Error('Lib is already defined');
+}
+
+
+window[namespace] = (function (alert) { // create library namespace and add dependencies namespaces
+
+  'use strict';
+
+
+  // ----------------------------------------
+  // Constants
+  // ----------------------------------------
+
+
+  const version = '0.0.1';
+
+
+  // ----------------------------------------
+  // Entities
+  // ----------------------------------------
+
+
+
+  // ----------------------------------------
+  // Services
+  // ----------------------------------------
+
+
+
+  // ----------------------------------------
+  // Adapters
+  // ----------------------------------------
+
+
+  function greet() {
+    alert('Hello, World!');
+  }
+
+
+  // ----------------------------------------
+  // Exports
+  // ----------------------------------------
+
+
+  return { version, greet };
+
+
+  // ----------------------------------------
+
+
+})(
+  window.alert // add dependency injection
+);
+
+// usage: <script> window.Lib.greet(); </script>
