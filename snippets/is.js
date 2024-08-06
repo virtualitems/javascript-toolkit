261bc55e5f9f1bd9c369661a0f9a8fcc88a9dff5
Alejandro CR <contacto@alejandrocr.co>
2024-08-06 17:06:51 -0500

    replace is-valuable with is.js

--- snippets/is.js
+++ snippets/is.js
@@ -1,30 +1,7 @@
 /**
- * Check if a value is valuable
- *
- * @param {any} value
- * @returns {Boolean}
- */
-function valuable(value) {
-
-  if (value === undefined) {
-    return false;
-  }
-
-  if (value === null) {
-    return false;
-  }
-
-  if (Object.is(value, NaN)) {
-    return false;
-  }
-
-  return true;
-
-};
-
-
-/**
- * @description Holds a set of functions to check the type of a value using the "typeof" operator
+ * @description
+ * Holds a set of functions to check the type of a value.
+ * It only checks the type of the value, not the value itself.
  */
 const is = {
   array: Array.isArray,
@@ -32,26 +9,27 @@ const is = {
   boolean: value => 'boolean' === typeof value,
   finite: Number.isFinite,
   function: value => 'function' === typeof value,
-  instanceOf: (value, constructor) => value instanceof constructor,
+  instanceOf: (constructor, value) => value instanceof constructor,
   integer: Number.isInteger,
   nan: value => Object.is(value, NaN),
   number: value => 'number' === typeof value,
   object: value => value !== null && 'object' === typeof value,
+  ref: Object.is,
   string: value => 'string' === typeof value,
   symbol: value => 'symbol' === typeof value,
-  valuable: value => value !== undefined && value !== null && !Object.is(value, NaN),
   not: {
     array: value => !Array.isArray(value),
     bigint: value => 'bigint' !== typeof value,
     boolean: value => 'boolean' !== typeof value,
     finite: value => !Number.isFinite(value),
     function: value => 'function' !== typeof value,
+    instanceOf: (constructor, value) => !(value instanceof constructor),
     integer: value => !Number.isInteger(value),
     nan: value => !Object.is(value, NaN),
     number: value => 'number' !== typeof value,
     object: value => value === null || 'object' !== typeof value,
+    ref: (value, other) => !Object.is(value, other),
     string: value => 'string' !== typeof value,
-    symbol: value => 'symbol' !== typeof value,
-    valuable: value => value === undefined || value === null || Object.is(value, NaN)
+    symbol: value => 'symbol' !== typeof value
   }
 };
