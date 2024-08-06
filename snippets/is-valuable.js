89ed7e37ba7032727c74b68b2827406d51a9b727
Alejandro CR <contacto@alejandrocr.co>
2024-08-06 05:08:45 -0500

    refactor isValuable function and add new type checking functions

--- snippets/is-valuable.js
+++ snippets/is-valuable.js
@@ -27,12 +27,22 @@ function valuable(value) {
  * @description Holds a set of functions to check the type of a value using the "typeof" operator
  */
 const is = {
-  valuable: (value) => (value !== undefined) && (value !== null) && !Object.is(value, NaN),
-  object: (value) => (value !== null) && ('object' === typeof value),
-  boolean: (value) => ('boolean' === typeof value),
-  number: (value) => (!Object.is(value, NaN) && ('number' === typeof value)),
-  bigint: (value) => ('bigint' === typeof value),
-  string: (value) => ('string' === typeof value),
-  symbol: (value) => ('symbol' === typeof value),
-  function: (value) => ('function' === typeof value),
+  valuable: value => (value !== undefined) && (value !== null) && !Object.is(value, NaN),
+  object: value => (value !== null) && ('object' === typeof value),
+  boolean: value => ('boolean' === typeof value),
+  number: value => (!Object.is(value, NaN) && ('number' === typeof value)),
+  bigint: value => ('bigint' === typeof value),
+  string: value => ('string' === typeof value),
+  symbol: value => ('symbol' === typeof value),
+  function: value => ('function' === typeof value),
+  not: {
+    valuable: value => (value === undefined) || (value === null) || Object.is(value, NaN),
+    object: value => (value === null) || ('object' !== typeof value),
+    boolean: value => ('boolean' !== typeof value),
+    number: value => Object.is(value, NaN) || ('number' !== typeof value),
+    bigint: value => ('bigint' !== typeof value),
+    string: value => ('string' !== typeof value),
+    symbol: value => ('symbol' !== typeof value),
+    function: value => ('function' !== typeof value),
+  },
 };
