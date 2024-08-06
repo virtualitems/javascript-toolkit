19785af5bad7934dc09cb95f973d3d397670e6ac
Alejandro CR <contacto@alejandrocr.co>
2024-08-06 04:46:59 -0500

    refactor isValuable function and add new type checking functions

--- snippets/is-valuable.js
+++ snippets/is-valuable.js
@@ -4,7 +4,7 @@
  * @param {any} value
  * @returns {Boolean}
  */
-const isValuable = function(value) {
+function valuable(value) {
 
   if (value === undefined) {
     return false;
@@ -14,10 +14,25 @@ const isValuable = function(value) {
     return false;
   }
 
-  if(Object.is(value, NaN)) {
+  if (Object.is(value, NaN)) {
     return false;
   }
 
   return true;
 
+};
+
+
+/**
+ * @description Holds a set of functions to check the type of a value using the "typeof" operator
+ */
+const is = {
+  valuable: (value) => (value !== undefined) && (value !== null) && !Object.is(value, NaN),
+  object: (value) => (value !== null) && ('object' === typeof value),
+  boolean: (value) => ('boolean' === typeof value),
+  number: (value) => (!Object.is(value, NaN) && ('number' === typeof value)),
+  bigint: (value) => ('bigint' === typeof value),
+  string: (value) => ('string' === typeof value),
+  symbol: (value) => ('symbol' === typeof value),
+  function: (value) => ('function' === typeof value),
 };
