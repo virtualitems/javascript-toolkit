28538869528baef0cafde0ad8d4dfdd8fac20741
Virtual Items <virtualitemsuniverse@gmail.com>
2024-08-15 01:30:07 -0500

    update component.jsx

--- templates/component.jsx
+++ templates/component.jsx
@@ -2,17 +2,13 @@
 
 import React from 'react';
 
-// Shared Module
+// Components
 
-// Other Modules
+// Hooks
 
-// Same Layer
-
-// Lower Layers
+// Services
 
 // Types
-
-// Interfaces
 
 // Constants
 
@@ -20,7 +16,7 @@ import React from 'react';
 /**
  * @description 
  *
- * @param {React.HTMLAttributes<HTMLElement>} props
+ * @param {React.HTMLProps<HTMLElement>} props
  * @returns {React.ReactElement}
  */
 export default function __COMPONENT__(props) {
