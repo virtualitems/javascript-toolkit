0da3fb2a15bb54beb4d93666aad982c357f81b98
Virtual Items <virtualitemsuniverse@gmail.com>
2025-02-05 13:30:49 -0500

    Update create-html-element.js

--- snippets/create-html-element.js
+++ snippets/create-html-element.js
@@ -1,45 +1,113 @@
 /**
- * Creates a new DOM element with the specified tagName, properties, and children.
+ * @fileoverview Browser library template
+ * @version 1.0.0
  *
- * @param {string} tagName - The tag name of the element to create. If the tagName is an empty string, a document fragment will be created.
- * @param {Object} [props] - An object containing properties to set on the element.
- * @param {Array<string|HTMLElement|DocumentFragment>} [children] - An array of children to append to the element.
- *
- * @returns {HTMLElement|DocumentFragment} The created DOM element or document fragment.
- *
- * @throws {Error} If the tagName is not a string.
- * @throws {Error} If the tagName contains invalid characters.
- * @throws {Error} If props is not an object, null, or undefined.
+ * @example [html] <script> window.Lib.greet(); </script>
  */
-function createElement(tagName, props, ...children) {
 
-  if ('string' !== typeof tagName) {
-    throw new Error('Type must be a string');
+
+// ----------------------------------------
+// Checks
+// ----------------------------------------
+
+
+// ----------------------------------------
+// Namespaces
+// ----------------------------------------
+
+const namespace = 'GUI';
+
+
+if (namespace in window) {
+  throw new Error(`"${namespace}" already defined in window`);
+}
+
+
+window[namespace] = (function (window) {
+
+  'use strict';
+
+  const document = window.document;
+  const stateMap = new WeakMap();
+
+  /**
+   * Appends an array of children to a parent node.
+   *
+   * @param {Node} parent - The parent node to which the children will be appended.
+   * @param {Array<Node|string>} children - An array of child nodes or strings to be appended to the parent.
+   *
+   * @throws {Error} Throws an error if the parent is not a Node.
+   */
+  function appendChildren(parent, children) {
+
+    if (!(parent instanceof Node)) {
+      throw new Error('Parent must be a Node');
+    }
+
+    for (const child of children) {
+      parent.appendChild(
+        child instanceof Node
+          ? child
+          : document.createTextNode(child)
+      );
+    }
   }
 
-  tagName = tagName.trim();
 
-  if (!/^[a-zA-Z0-9-]*$/.test(tagName)) {
-    throw new Error('Tag name must contain only letters, numbers, hyphens, or be an empty string.');
+  /**
+   * Creates a DocumentFragment and appends the provided children to it.
+   *
+   * @param {Array<Node>} children - The child nodes to append to the DocumentFragment.
+   *
+   * @returns {DocumentFragment} The created DocumentFragment with the appended children.
+   */
+  function createDocumentFragment(...children) {
+    const fragment = document.createDocumentFragment();
+    appendChildren(fragment, children);
+    return fragment;
   }
 
-  const arePropsDefined = (props !== undefined && props !== null);
 
-  if (arePropsDefined && props.constructor !== Object) {
-    throw new Error('Props must be a plain object or null.');
-  }
+  /**
+   * Creates a new DOM element with the specified tagName, properties, and children.
+   *
+   * @param {string} tagName - The tag name of the element to create. If the tagName is an empty string, a document fragment will be created.
+   * @param {Object} [props] - An object containing properties to set on the element.
+   * @param {Array<string|HTMLElement|DocumentFragment>} [children] - An array of children to append to the element.
+   *
+   * @returns {HTMLElement|DocumentFragment} The created DOM element or document fragment.
+   *
+   * @throws {Error} If the tagName is not a string.
+   * @throws {Error} If the tagName contains invalid characters.
+   * @throws {Error} If props is not an object, null, or undefined.
+   */
+  function createElement(tagName, props, ...children) {
 
-  props = props ?? {};
+    if (tagName === '') {
+      return createDocumentFragment(...children);
+    }
 
-  let element = null;
+    if ('string' !== typeof tagName) {
+      throw new Error('Type must be a string');
+    }
 
-  if (tagName === '') {
-    element = document.createDocumentFragment();
+    if (!/^[a-zA-Z0-9-]*$/.test(tagName)) {
+      throw new Error('Tag name must contain only letters, numbers, hyphens, or be an empty string.');
+    }
 
-  } else {
-    element = document.createElement(tagName);
+    let element = document.createElement(tagName);
 
-    for (key in props) {
+    appendChildren(element, children);
+
+    if (props === undefined || props === null) {
+      return element;
+    }
+
+    if (props.constructor !== Object) {
+      throw new Error('Props must be a plain object or null.');
+    }
+
+    for (const key in props) {
 
       if (props[key]?.constructor === Object) {
         Object.assign(element[key], props[key]);
@@ -49,15 +117,53 @@ function createElement(tagName, props, ...children) {
       element[key] = props[key];
     }
 
+    return element;
   }
 
-  for (child of children) {
-    element.appendChild(
-      child instanceof Node
-        ? child
-        : document.createTextNode(child)
-    );
+
+  /**
+   * Sets the state for a given element.
+   *
+   * @param {HTMLElement} element - The element for which to set the state.
+   * @param {Object} state - The state to set for the element.
+   */
+  function setState(element, state) {
+    if (!(element instanceof Node)) {
+      throw new Error('Element must be a Node');
+    }
+
+    stateMap.set(element, state);
   }
 
-  return element;
-}
+  /**
+   * Gets the state for a given element.
+   *
+   * @param {Node} element - The element for which to get the state.
+   * @returns {any} The state of the element, or undefined if no state is set.
+   */
+  function getState(element) {
+    if (!(element instanceof Node)) {
+      throw new Error('Element must be a Node');
+    }
+
+    return stateMap.get(element);
+  }
+
+
+  // ----------------------------------------
+  // Exports
+  // ----------------------------------------
+
+  return {
+    createDocumentFragment,
+    createElement,
+    getState,
+    setState
+  };
+
+
+  // ----------------------------------------
+  // Imports
+  // ----------------------------------------
+
+})(window);
