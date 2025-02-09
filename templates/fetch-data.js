cc091573cbd41447f26eb5a8012e875881560f28
Alejandro CR <contacto@alejandrocr.co>
2025-02-09 00:24:15 -0500

    Add fetch-data.js to retrieve and display user data from API

---
+++ templates/fetch-data.js
@@ -0,0 +1,77 @@
+(async () => {
+
+  class UserModel {
+
+    constructor() {
+      this.id = null;
+      this.email = null;
+      this.firstName = null;
+      this.lastName = null;
+      this.avatar = null;
+    }
+
+    get fullName() {
+      return [this.firstName, this.lastName].filter(Boolean).join(' ');
+    }
+
+  }
+
+  const requestTarget = new URL(`https://reqres.in/api/users`);
+
+  const headers = new Headers({
+    'Cache-Control': 'no-store',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  });
+
+  const requestOptions = {
+    method: 'GET',
+    cors: 'no-cors',
+    headers,
+  };
+
+  // request phase
+  const response = await fetch(requestTarget, requestOptions);
+
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+
+  // decode phase
+  const json = await response.json();
+
+  const page = json.page;
+  const perPage = json.per_page;
+  const total = json.total;
+  const totalPages = json.total_pages;
+  const data = json.data;
+
+  // transform phase
+  const users = data.map(user => {
+    const userModel = new UserModel();
+    userModel.id = user.id;
+    userModel.email = user.email;
+    userModel.firstName = user.first_name;
+    userModel.lastName = user.last_name;
+    userModel.avatar = user.avatar;
+    return userModel;
+  });
+
+  // use phase
+  let pElement = document.body.appendChild(document.createElement('p'));
+  pElement.innerHTML = `Page: ${page}<br>Per Page: ${perPage}<br>Total: ${total}<br>Total Pages: ${totalPages}`;
+
+  for (const user of users) {
+    const div = document.body.appendChild(document.createElement('div'));
+
+    const img = div.appendChild(document.createElement('img'));
+    img.src = user.avatar;
+
+    const p1 = div.appendChild(document.createElement('p'));
+    p1.textContent = user.fullName;
+
+    const p2 = div.appendChild(document.createElement('p'));
+    p2.textContent = user.email;
+  }
+
+})();
