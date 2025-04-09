0f25a91fdc558e627b392fbde428a4b3590ada7e
Alejandro CR <contacto@alejandrocr.co>
2025-04-09 02:29:21 -0500

    refactor: improve type definitions and error handling in ErrorBoundary component

--- templates/reactjs/ErrorBoundary.js
+++ templates/reactjs/ErrorBoundary.js
@@ -1,74 +1,89 @@
+import React from 'react';
+
 /**
- * @typedef _TProps
- * @type {object}
- * @property {React.ReactElement} fallback
- * @property {(error: Error, errorInfo: React.ErrorInfo) => void} componentDidCatch
- *
- * @typedef TProps
- * @type {import('react').HTMLAttributes<HTMLDivElement> & _TProps}
+ * @typedef Props
+ * @type {import('react').PropsWithChildren<HTMLElement> & {
+ *   fallback: React.ReactElement,
+ *   onError: (error: Error, errorInfo: React.ErrorInfo) => void
+ * }}
  */
 
 /**
- * @typedef TState
+ * @typedef State
  * @type {object}
  * @property {Error | null} error
  */
 
 /**
- * @description 
+ * @description Initial state of the ErrorBoundary component
  */
-export default class ErrorBoundary extends React.Component
-{
+const initialState = {
+  error: null,
+};
 
-  /** @type {TProps} */
+/**
+ * @description React Error Boundary Component
+ */
+export class ErrorBoundary extends React.Component {
+  /** @type {Props} */
   props;
 
-  /** @type {TState} */
+  /** @type {State} */
   state;
 
   /**
-   * @param {TProps} props
+   * @param {Props} props
    */
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
-    this.state = { error: null };
+
+    const { fallback, onError } = props;
+
+    if (typeof fallback !== 'function') {
+      throw new TypeError('Fallback must be a function');
+    }
+
+    if (typeof onError !== 'function') {
+      throw new TypeError('onError must be a function');
+    }
+
+    this.state = initialState;
+    this.resetErrorBoundary = this.resetErrorBoundary.bind(this);
   }
 
   /**
    * @param {Error} error
-   * @returns {TState}
+   * @returns {State}
    */
-  static getDerivedStateFromError(error)
-  {
+  static getDerivedStateFromError(error) {
     return { error };
   }
 
   /**
    * @param {Error} error
-   * @param {import('react').ErrorInfo} errorInfo
+   * @param {import('react').ErrorInfo} info
    */
-  componentDidCatch(error, errorInfo)
-  {
-    this.props.componentDidCatch(error, errorInfo);
+  componentDidCatch(error, info) {
+    this.props.onError(error, info);
   }
 
-  resetErrorBoundary()
-  {
-    this.setState({ error: null });
+  resetErrorBoundary() {
+    this.setState(initialState);
   }
 
   /**
    * @returns {import('react').ReactElement}
    */
-  render()
-  {
-    if (this.state.error === null) {
+  render() {
+    const { error } = this.state;
+
+    if (error === null) {
       return this.props.children;
     }
 
-    return React.createElement(this.props.fallback, { error: this.state.error });
-
+    return React.createElement(this.props.fallback, {
+      error: error,
+      resetErrorBoundary: this.resetErrorBoundary,
+    });
   }
-
 }
