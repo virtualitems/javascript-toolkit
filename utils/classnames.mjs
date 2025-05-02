c95ce3580b6439fb770603f5e629055fba7f956c
Alejandro CR <contacto@alejandrocr.co>
2025-05-01 20:39:31 -0500

    feat: add classNames utility function for joining class names

---
+++ utils/classnames.mjs
@@ -0,0 +1,38 @@
+/**
+ * Function to join class names together.
+ * @param  {unknown[]} args - The class names to join.
+ */
+export function classNames(...args) {
+  const classNames = new Set();
+
+  for (const arg of args) {
+    if (arg === null || arg === undefined) {
+      continue;
+    }
+
+    if (arg.constructor === String) {
+      classNames.add(arg);
+      continue;
+    }
+
+    if (arg.constructor === Object) {
+      for (const key in arg) {
+        if (arg[key] && 'string' === typeof key) {
+          classNames.add(key);
+        }
+      }
+      continue;
+    }
+
+    if ('function' === typeof arg[Symbol.iterator]) {
+      for (const item of arg) {
+        if ('string' === typeof item) {
+          classNames.add(item);
+        }
+      }
+      continue;
+    }
+  }
+
+  return Array.from(classNames).join(' ');
+}
