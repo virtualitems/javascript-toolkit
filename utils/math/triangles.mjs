696f823393d8787e0346c2f1fc49f428dc4a658f
Alejandro CR <virtualitemsuniverse@gmail.com>
2025-12-24 18:27:21 -0500

    feat: refactor Triangle class to remove constructor and angle validation methods

--- utils/math/triangles.mjs
+++ utils/math/triangles.mjs
@@ -1,34 +1,34 @@
 export class Triangle {
-  constructor(angle1, angle2, angle3) {
 
-    const angle1IsNumber = 'number' === typeof angle1;
-    const angle2IsNumber = 'number' === typeof angle2;
-    const angle3IsNumber = 'number' === typeof angle3;
+  static area(base, height) {
+    return 0.5 * base * height;
+  }
 
-    if ((angle1 !== null) && (!angle1IsNumber || angle1 < 0)) {
-      throw new TypeError('angle1 must be a positive number or null');
-    }
+  static perimeter(sideA, sideB, sideC) {
+    return sideA + sideB + sideC;
+  }
 
-    if ((angle2 !== null) && (!angle2IsNumber || angle2 < 0)) {
-      throw new TypeError('angle2 must be a positive number or null');
-    }
+  static sin(opposite, hypotenuse) {
+    return opposite / hypotenuse;
+  }
 
-    if ((angle3 !== null) && (!angle3IsNumber || angle3 < 0)) {
-      throw new TypeError('angle3 must be a positive number or null');
-    }
+  static cos(adjacent, hypotenuse) {
+    return adjacent / hypotenuse;
+  }
 
-    const angleSum = (angle1 ?? 0) + (angle2 ?? 0) + (angle3 ?? 0);
+  static tan(opposite, adjacent) {
+    return opposite / adjacent;
+  }
 
-    if (angle1IsNumber && angle2IsNumber && angle3IsNumber && angleSum !== 180) {
-      throw new Error('The sum of the angles in a triangle must be 180 degrees');
-    }
+  static cot(adjacent, opposite) {
+    return adjacent / opposite;
+  }
 
-    if (angleSum >= 180) {
-      throw new Error('The sum of the angles in a triangle must be less than 180 degrees');
-    }
+  static sec(hypotenuse, adjacent) {
+    return hypotenuse / adjacent;
+  }
 
-    this.angle1 = angle1;
-    this.angle2 = angle2;
-    this.angle3 = angle3;
+  static csc(hypotenuse, opposite) {
+    return hypotenuse / opposite;
   }
 }
