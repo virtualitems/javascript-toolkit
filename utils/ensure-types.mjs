203eee34eb5c73a48e941063f6fd02abe5d714ec
Alejandro CR <virtualitemsuniverse@gmail.com>
2026-01-11 03:50:06 -0500

    feat: add function to ensure all provided operators are of the same type

---
+++ utils/ensure-types.mjs
@@ -0,0 +1,51 @@
+/**
+ *  Ensures that all provided operators are of the same type.
+ *
+ * @param  {...unknown} operators
+ * @returns {boolean}
+ *
+ * @example
+ * e(1, 2, 3); // true
+ * e('a', 'b', 'c'); // true
+ * e([], {}, []); // false
+ *
+ * @example
+ * const result = e(1, '2', 3) ? 1 + 2 + 3 : 0;
+ * console.log(result); // 0
+ */
+export function e(...operators) {
+
+  if (operators.length < 2) {
+    throw new Error('At least two operators are required');
+  }
+
+  const length = operators.length;
+  const firstElement = operators[0];
+
+  if (firstElement === undefined) {
+    for (let i = 1; i < length; i++)
+      if (operators[i] !== undefined)
+        return false;
+    return true;
+  }
+
+  if (firstElement === null) {
+    for (let i = 1; i < length; i++)
+      if (operators[i] !== null)
+        return false;
+    return true;
+  }
+
+  if (Object.is(firstElement, NaN)) {
+    for (let i = 1; i < length; i++)
+      if (Object.is(operators[i], NaN) === false)
+        return false;
+    return true;
+  }
+
+  for (let i = 1; i < length; i++)
+    if (operators[i]?.constructor !== firstElement.constructor)
+      return false;
+
+  return true;
+}
