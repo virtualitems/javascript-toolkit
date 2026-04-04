95babc70f4fe987087a53d2aaa3b63d3e1c69588
Alejandro CR <virtualitemsuniverse@gmail.com>
2026-04-04 02:30:51 -0500

    add example for batch iterations

---
+++ examples/batch-iterations.mjs
@@ -0,0 +1,24 @@
+const csv = [
+  ['item', 'Price', 'Quantity'],
+  ['Item A', 10, 2],
+  ['Item B', 15, 5],
+  ['Item C', 20, 3],
+  ['Item D', 25, 4]
+]
+
+function makeInvoice(rows) {
+  const invoice = {
+    items: new Array(rows.length),
+    total: 0
+  }
+
+  for (let i = 0, end = rows.length; i < end; i += 1) {
+    const [item, price, quantity] = rows[i]
+    invoice.items[i] = { item, price, quantity, total: price * quantity }
+    invoice.total += invoice.items[i].total
+  }
+
+  return invoice
+}
+
+console.log(makeInvoice(csv.slice(1)))
