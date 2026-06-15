1bed7ce4d4029db8636746d3a5d1ebdbbceace21
Virtual Items <virtualitemsuniverse@gmail.com>
2026-06-14 21:43:35 -0500

    Refactor Result type for better clarity and usage

--- utils/typescript/results.ts
+++ utils/typescript/results.ts
@@ -1,6 +1,5 @@
-export type Result<T = unknown> = {
-  value?: T
-  failure?: string
-}
+type Couplet<T = unknown> = [T] | [T, undefined] | [undefined, string]
 
-export type Couplet<T = unknown> = [T] | [T, undefined] | [undefined, string]
+type Result<T = unknown> =
+  | { value: T; failure?: undefined }
+  | { value?: undefined; failure: string }
