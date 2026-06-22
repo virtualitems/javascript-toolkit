c98c829857d4244c34c41157535fd724fa5756a5
Alejandro CR <virtualitemsuniverse@gmail.com>
2026-06-22 02:29:02 -0500

    feat(chars): reorganize control characters and punctuation constants for clarity

--- utils/chars.mjs
+++ utils/chars.mjs
@@ -1,20 +1,50 @@
 /* Control characters */
-export const NUL = 0x00 // \0 - null character
-export const BEL = 0x07 // \a - alert bell
-export const BSP = 0x08 // \b - backspace
-export const ESC = 0x1b // \e - escape
-export const FPB = 0x0c // \f - formfeed page break
-export const LFD = 0x0a // \n - line feed
-export const CRR = 0x0d // \r - carriage return
-export const HTB = 0x09 // \t - horizontal tab
-export const VTB = 0x0b // \v - vertical tab
-
-/* Punctuation */
-export const BSL = 0x5c // \\
-export const APO = 0x27 // \'
-export const DQT = 0x22 // \"
-export const QMK = 0x3f // \?
+export const BEL = 0x07 // \a alert bell
+export const BS = 0x08 // \b backspace
+export const CR = 0x0d // \r carriage return
+export const CRLF = 0x0d0a // \r\n carriage return + line feed
+export const ESC = 0x1b // \e escape
+export const FF = 0x0c // \f formfeed page break
+export const HT = 0x09 // \t horizontal tab
+export const LF = 0x0a // \n line feed
+export const NUL = 0x00 // \0 null character
+export const VT = 0x0b // \v vertical tab
 
 /* Whitespace */
-export const SPC = 0x20 // space
-export const NBS = 0xa0 // non-breaking space
+export const NBSP = 0xa0 // non-breaking space
+export const SPACE = 0x20 // space
+
+/* ASCII symbols */
+export const AMP = 0x26 // & ampersand
+export const APOS = 0x27 // ' apostrophe
+export const AST = 0x2a // * asterisk
+export const BQK = 0x60 // ` backquote
+export const BSOL = 0x5c // \ backslash
+export const COLON = 0x3a // : colon
+export const COMMA = 0x2c // , comma
+export const COMMAT = 0x40 // @ at sign
+export const DOLLAR = 0x24 // $ dollar sign
+export const EQUALS = 0x3d // = equals sign
+export const EXCL = 0x21 // ! exclamation mark
+export const GRAVE = 0x60 // ` backtick
+export const GT = 0x3e // > greater-than sign
+export const HAT = 0x5e // ^ caret
+export const HYPHEN_MINUS = 0x2d // - hyphen-minus
+export const LBRACE = 0x7b // { left curly brace
+export const LBRACK = 0x5b // [ left square bracket
+export const LOWBAR = 0x5f // _ underscore
+export const LPAR = 0x28 // ( left parenthesis
+export const LT = 0x3c // < less-than sign
+export const NUM = 0x23 // # number sign
+export const PERCNT = 0x25 // % percent sign
+export const PERIOD = 0x2e // . period
+export const PLUS = 0x2b // + plus sign
+export const QUEST = 0x3f // ? question mark
+export const QUOT = 0x22 // " double quote
+export const RBRACE = 0x7d // } right curly brace
+export const RBRACK = 0x5d // ] right square bracket
+export const RPAR = 0x29 // ) right parenthesis
+export const SEMI = 0x3b // ; semicolon
+export const SOL = 0x2f // / forward slash
+export const TILDE = 0x7e // ~ tilde
+export const VERT = 0x7c // | vertical bar
