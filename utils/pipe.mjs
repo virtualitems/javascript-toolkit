8119531c178e32530e9aa031d76ebe3ca10e6a8d
Alejandro CR <virtualitemsuniverse@gmail.com>
2026-06-22 02:08:37 -0500

    feat(pipe): refactor pipe function to improve argument handling and simplify implementation

--- utils/pipe.mjs
+++ utils/pipe.mjs
@@ -1,22 +1,23 @@
 /**
+ * @param {unknown} arg
  * @param {Function[]} fns
- * @param {unknown} thisArg
- * @param {unknown[]} args
+ *
+ * @example
+ * const add = (a, b) => a + b
+ * const double = (x) => x * 2
+ *
+ * pipe(1, (x) => add(x, 2), double) // => 6
  */
-export function pipe(fns, thisArg = null, args = undefined) {
-  let result = null
+export function pipe(arg, ...fns) {
+  let result = arg
 
-  if (Array.isArray(fns) === false || fns.some((fn) => 'function' !== typeof fn)) {
-    throw new TypeError('Expected functions array')
-  }
+  if (fns.length === 0) throw new TypeError('Expected at least one function')
 
-  if (args !== undefined && Array.isArray(args) === false) {
-    throw new TypeError('Expected args to be an array')
-  }
+  if (fns.some((fn) => 'function' !== typeof fn))
+    throw new TypeError('Expected all arguments after the first to be functions')
 
   for (const fn of fns) {
-    result = fn.apply(thisArg, args)
-    args = [result]
+    result = fn.call(null, result)
   }
 
   return result
