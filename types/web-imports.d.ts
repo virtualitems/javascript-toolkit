35e5970430e0c7462554600eaf9dd0bd85ba2a81
Alejandro CR <virtualitemsuniverse@gmail.com>
2026-07-01 04:10:16 -0500

    update imports

---
+++ types/web-imports.d.ts
@@ -0,0 +1,11 @@
+declare module 'https://esm.sh/@reduxjs/toolkit' {
+  export * from '@reduxjs/toolkit'
+}
+
+declare module 'https://esm.sh/react' {
+  export * from 'react'
+}
+
+declare module 'https://esm.sh/react-dom/client' {
+  export * from 'react-dom/client'
+}
